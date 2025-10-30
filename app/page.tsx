864fba21dd739eeec313e7e9147b09544db3fb6e
skirk6 <154077006+skirk6@users.noreply.github.com>
2025-10-29 20:46:28 -0400

    chore: bootstrap Next.js app

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,7 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+		<h1 className="text-3xl font-bold">In One We Trust</h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
             <a
